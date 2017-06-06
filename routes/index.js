aef44da4dfedee5d7b9932e0d902785615d09364
Ian Booton <ianbooton@Ians-MacBook-Pro.local>
2017-06-06 15:38:50 -0400

    encryption

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var config = require('../config/config')
+var bcrypt = require('bcrypt-nodejs');
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
@@ -21,11 +22,14 @@ const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 router.get('/', function(req, res, next) {
 	request.get(nowPlayingUrl, (error, response,movieData)=>{
 		var movieData = JSON.parse(movieData);
+		// console.log('+++++++++++++++++++++++++')
+		// console.log(req.session)
+		// console.log('+++++++++++++++++++++++++')
 		res.render('movie-list', {
 			movieData: movieData.results,
 			imageBaseUrl: imageBaseUrl,
-			titleHeader: "Welcome to my movie app. These are now now playing:"
-
+			titleHeader: "Welcome to my movie app. These are now now playing:",
+			sessionInfo: req.session
 		});
 	})
 });
@@ -70,6 +74,74 @@ router.get('/movie/:id', (req, res)=>{
 	})
 
 })
+
+router.get('/signup', function(req,res){
+	var message = req.query.msg;
+	if (message == 'badEmail'){
+		message = 'This email is already in use'
+	}
+	res.render('signup', {message: message})
+})
+
+router.post('/signupProcess', function(req,res){
+	// res.json(req.body)
+	var name = req.body.name
+	var email = req.body.email
+	var password = req.body.password
+	var hash = bcrypt.hashSync(password);
+	console.log(hash)
+
+	var selectQuery = "SELECT * FROM users WHERE email = ?";
+	connection.query(selectQuery,[email], function(error, results){
+		if(results.length == 0){
+			var insertQuery = "INSERT INTO users (name,email,password) VALUES (?,?,?)";
+			connection.query(insertQuery,[name,email,hash], function(error,results){
+				req.session.name = name;
+				req.session.email = email;
+				req.session.loggedin = true;
+				res.redirect('/?msg=registered')
+			})
+		}else{
+			res.redirect('/signup?msg=badEmail')
+		}
+
+	})
+
+	
+})
+
+router.get('/login', function(req,res){
+	res.render('login')
+
+})
+
+router.post('/processLogin', function(req,res){
+	var email = req.body.email
+	var password = req.body.password
+	// var selectQuery = "SELECT * FROM users WHERE email = ? AND password = ?";
+	var selectQuery = "SELECT * FROM users WHERE email = ?";
+
+	connection.query(selectQuery, [email], function(error,results){
+		if (results.length == 1){
+			// Match found
+			var match = bcrypt.compareSync(password, results[0].password)
+			if(match === true){
+				req.session.loggedin = true;
+				req.session.name = results.name;
+				req.session.email = results.email;
+				res.redirect('/?msg=loggedin')
+
+		}else{
+			es.redirect('/login?msg=badLogin')
+		}
+
+			
+		}else{
+			res.redirect('/login?msg=badLogin')
+		}
+	})
+})
+
 
 
 
